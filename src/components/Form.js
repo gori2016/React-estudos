eef77f1e87abd23d8d4bef6af79203b02e53e9c2
gori2016 <igorborgesaraujo1677@gmail.com>
2025-11-16 12:43:58 -0300

    Ajustar o componente Form: adicionar estado para o nome e melhorar a função de envio

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,17 +1,28 @@
-function Form(){
+import {use, useState} from 'react'
 
-function Enviar(){
-    return(alert("Formulário enviado com sucesso!")
-)
-}
+function Form() {
 
+    function Enviar(e) {
+     (alert("Formulário enviado com sucesso!"))
+    console.log(`O nome é: ${nome}`)
+    e.preventDefault()
+    }
 
-    return(
+    const [nome, setName ] = useState()
+
+    return (
         <div>
             <h1>Meu Cadastro</h1>
             <form onSubmit={Enviar}>
                 <label htmlFor="">Nome</label>
-                <input type="text" />
+                <input type="text" id='name' name='name' onChange={(e) => setName(e.target.value)} />
+                
+                <div>
+                <label htmlFor="">Senha</label>
+                <input type="password" name="" id="" />
+                </div>
+
+
                 <input type="submit" value="enviar" />
             </form>
         </div>
